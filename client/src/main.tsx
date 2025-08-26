2e02d35abfc7170992e40c59bd2fed3ccb936bb5
Rusiru Erandaka <erandak-ec20037@stu.kln.ac.lk>
2025-08-26 18:43:02 +0530

    updates for json files

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
+import React from 'react'
 import { createRoot } from 'react-dom/client'
+import App from './App'
 import './index.css'
-import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+const container = document.getElementById('root')
+if (!container) {
+  throw new Error('Root element not found')
+}
+
+const root = createRoot(container)
+root.render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
+  </React.StrictMode>
 )
